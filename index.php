11b9b8e84a8a84d69b1e04001d2efa5ea5650f1e
kongphop1054 <73678163+kongphop1054@users.noreply.github.com>
2020-10-30 10:36:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h1>Hello PHP</h1>";
-echo "Hello world!<br";
-?>
+<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>My First Heading</h1>
+    <p>My First Paragraph</p>
+    <img src="unnamed.jpg" />
+    <br />
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h2>The unordered list</h2>
+    <ul>
+      <l1>Google</l1>
+      <l2>Youtube</l2>
+      <l3>Twitter</l3>
+    </ul>
+  </body>
+</html>
