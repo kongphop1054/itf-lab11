7a2d4bf532c2639966973402cd381d4b8e6bf785
kongphop1054 <73678163+kongphop1054@users.noreply.github.com>
2020-11-06 16:26:59 +0700

    Create cardlab12.php

---
+++ cardlab12.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Hello</title>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+   <div class="container">
+       <div class="row">
+           <div class="col-sm-3">
+               <div class="card">
+                   <div class="card-header bg-danger text-white" >
+                       <img src="https://picsum.photos/id/237/200/300" class="img-fluid rounded" />
+                   </div>
+                   <div class="card-body">
+                   <p>The black dog.</p>
+                   </div>
+                   <div class="card-footer">
+                   By kongphop
+                   </div>
+               </div>
+           </div>
+           <div class="col-sm-3">
+               <div class="card">
+                   <div class="card-header bg-danger text-white" >
+                       <img src="https://picsum.photos/id/507/200/300" class="img-fluid rounded" />
+                   </div>
+                   <div class="card-body">
+                   <p>A beautiful place.</p>
+                   </div>
+                   <div class="card-footer">
+                   By Naunsunthia
+                   </div>
+               </div>
+           </div>
+           <div class="col-sm-3">
+               <div class="card">
+                   <div class="card-header bg-danger text-white" >
+                       <img src="https://picsum.photos/id/1/200/300" class="img-fluid rounded" />
+                   </div>
+                   <div class="card-body">
+                   <p>working.</p>
+                   </div>
+                   <div class="card-footer">
+                   By 63070005
+                   </div>
+               </div>
+           </div>
+       </div> <!-- Close Row 1 -->
+   </div> <!-- Close container -->
+</body>
+</html>
