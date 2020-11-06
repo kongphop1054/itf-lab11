e04552632971a5fdc8ad977f6d284f7ab724537a
kongphop1054 <73678163+kongphop1054@users.noreply.github.com>
2020-11-06 12:08:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Hello</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
-  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-  <div class="container">
-    <h1>Hello</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio odit illum autem porro! Quisquam
-voluptatibus nesciunt impedit, suscipit corporis, minus culpa molestiae necessitatibus blanditiis,repellat
-mollitia beatae hic volupatatum deleniti.</p>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="img/work/work4.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-2">
+                <img src="img/work/work6.jpg" class="img-fluid">
+                <img src="img/work/work5.jpg" class="img-fluid">
+                <img src="img/work/work7.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <img src="img/work/work8.jpg" class="img-fluid">
+                <img src="img/work/work9.jpg" style="height: 20%;">
+            </div>
+        </div> 
+    </div>    
 </body>
 </html>
+
